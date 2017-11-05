7d1c8ed214e00729cfaded623dadd678506c1322
Webster4 <michal.stasinski@yahoo.com>
2017-11-05 11:12:04 +0100

    Program working on function

--- coin_throw.py
+++ coin_throw.py
@@ -3,8 +3,10 @@
 
 import random
 
-print("Hello in throw a coin simulation")
-print("\nComputer will throw a coin 100 times and counts how many times reverse and tail was drawn")
+# Function displays introduction to program
+def intro():
+	print("Hello in throw a coin simulation")
+	print("\nComputer will throw a coin 100 times and counts how many times reverse and tail was drawn")
 
 # This receive as parameters throw, tail and reverse counters
 # It returns how many times both sides of coin were drawn
@@ -17,22 +19,34 @@ def throw_counter(throws = 1, tail = 0, reverse = 0):
 			tail +=1
 		throws +=1
 	return tail, reverse
-		
 
+# Function returns a response yes or no on question in calling 
+def ask_yes_no(question):
+	response = None
+	while response not in ("y", "n"):
+		response = input(question).lower()
+	return response
+
+# Function asks player if he/she wants to play again
+def again():
+	if_again = ask_yes_no("\nDo you want play again (press y or n) ")
+	if if_again == "y":
+		print("\n\n")
+		main()
+	elif if_again == "n":
+		input("\n\nPlease press enter key if you want exit\n\n")
+
+# Function print the final result
+def result(tail, reverse):
+	print("\nThe coin was thrown 100 times. The score is:",reverse, "for reverse and ",tail,"for tail")
 	
-throws = 1
-reverse = 0
-tail = 0
-
-while throws != 101:
-	score = random.randint(0, 1)
-	if score:
-		reverse +=1
-	else: 
-		tail +=1
-	throws +=1
 	
-
-print("\nThe coin was thrown 100 times. The score is:",reverse, "for reverse and ",tail,"for tail")
-
-input("\n\nPlease press enter key if you want exit")
+	
+def main():
+	intro()
+	tail, reverse = throw_counter()
+	result(tail, reverse)
+	again()
+	
+# Starting of program
+main()
