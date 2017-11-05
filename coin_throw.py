385d8e36f57d0c76adc5bc0149f73549cc127b3c
Webster4 <michal.stasinski@yahoo.com>
2017-11-05 10:37:30 +0100

    Translated in English

--- coin_throw.py
+++ coin_throw.py
@@ -1,10 +1,13 @@
-# Symulacja rzutu monetą
-# Komputer zlicza ile razy wypadł orzeł a ile reszka
+# This is simulation of throw a coin
+# The program counts how many times reverse and tail was drawn in 100 attempts
 
 import random
 
-print("Program rzuci 100 razy monetą i wyliczy ile razy wypadła reszka, a ile orzeł")
+print("Hello in throw a coin simulation")
+print("\nComputer will throw a coin 100 times and counts how many times reverse and tail was drawn")
 
+# This receive as parameters throw, tail and reverse counters
+# It returns how many times both sides of coin were drawn
 def throw_counter(throws = 1, tail = 0, reverse = 0):
 	while throws != 101:
 		score = random.randint(0, 1)
@@ -15,7 +18,8 @@ def throw_counter(throws = 1, tail = 0, reverse = 0):
 		throws +=1
 	return tail, reverse
 		
-		
+
+	
 throws = 1
 reverse = 0
 tail = 0
@@ -28,6 +32,7 @@ while throws != 101:
 		tail +=1
 	throws +=1
 	
-print("\nProgram rzucił monetą 100 razy. Orzeł wypadł ", reverse, " razy, a reszka ", tail, " razy.")
 
-input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
+print("\nThe coin was thrown 100 times. The score is:",reverse, "for reverse and ",tail,"for tail")
+
+input("\n\nPlease press enter key if you want exit")
